023938b4d7a6a770c7688c53700027c6efe0e5b5
Jeverson Santos <109315416+JevSantos@users.noreply.github.com>
2026-05-22 07:38:29 -0300

    Enhance docker.sh for Docker installation
    Updated docker.sh to install Docker and Docker Compose with GPG key management.

--- docker.sh
+++ docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+apt-get update -y
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+usermod -aG docker vagrant#!/bin/bash
 curl -fsSL https://get.docker.com | sudo bash
 sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
